29cb69dbad1ae13e91475249a7ac267175f77728
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2024-03-29 23:50:34 +0300

    chore: simplify util function

--- src/planner/gcsa_services/gcsa_utils.py
+++ src/planner/gcsa_services/gcsa_utils.py
@@ -1,17 +1,15 @@
-import datetime
+from datetime import datetime, timedelta, date, time
 from typing import Tuple
 
 
-def create_week_interval() -> Tuple[datetime.datetime, datetime.datetime]:
+def create_week_interval() -> Tuple[datetime, datetime]:
     """
     Create one-week interval from Monday till Sunday
     :return: tuple of two datetime objects indicating the start and end
     """
-    now = datetime.datetime.now()
-    monday = datetime.date.today() - datetime.timedelta(days=now.weekday())
-    monday_midnight = datetime.time(hour=0, minute=00)
-    sunday = monday + datetime.timedelta(days=7)
-    sunday_midnight = datetime.time(hour=0, minute=00)
-    monday_datetime = datetime.datetime.combine(monday, monday_midnight)
-    sunday_datetime = datetime.datetime.combine(sunday, sunday_midnight)
+    monday = date.today() - timedelta(days=datetime.now().weekday())
+    midnight = time(hour=0, minute=00)
+    sunday = monday + timedelta(days=7)
+    monday_datetime = datetime.combine(monday, midnight)
+    sunday_datetime = datetime.combine(sunday, midnight)
     return monday_datetime, sunday_datetime
