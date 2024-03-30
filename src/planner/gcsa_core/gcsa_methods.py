957d467156f77e18e0d48a6dbae63a0f426e71e8
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2024-03-30 14:52:37 +0300

    chore: get event descriptions time

---
+++ src/planner/gcsa_core/gcsa_methods.py
@@ -0,0 +1,26 @@
+from typing import Dict
+from gcsa.google_calendar import GoogleCalendar
+
+from planner.gcsa_core.gcsa_utils import create_week_interval
+import locale
+
+# set up russian locale
+locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
+
+
+def get_weekly_upcoming_events(gc: GoogleCalendar) -> Dict[str, str | None]:
+    """
+    Retrieve all upcoming events for a specific gcsa_core week
+    :return: list of events for upcoming week
+    """
+    start, end = create_week_interval()
+    events = list(gc[start:end:"startTime"])
+    return {event.start.strftime('%A %d %B %H:%M').upper(): event.description for event in events}
+
+
+def get_daily_events(gc: GoogleCalendar) -> Dict[str, str | None]:
+    """
+    Retrieve all events for upcoming day
+    :return:
+    """
+    return None
